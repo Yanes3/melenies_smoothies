b605b275a202927924a30ac35d0b0d05404953d7
Cristian Yanes <76889311+Yanes3@users.noreply.github.com>
2025-04-16 15:49:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ingredients_list = st.multiselect(
 if ingredients_list:
   ingredients_string = ''
   
-  for fruit_chosen in ingredients_list:
+for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ''
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
@@ -42,7 +42,7 @@ if ingredients_list:
     
     time_to_insert = st.button('Submit Order')
     
-    if time_to_insert:
+if time_to_insert:
         # Ejecutar el comando SQL sin collect()
         session.sql(my_insert_stmt).collect()
         
